884cfac869e9052748cf95dd8507e10a5b6a20e5
LotusGit <lotuslindez@gmail.com>
2019-07-15 22:26:24 -0500

    added win condition and timer

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
+//  Variable that will hold our setInterval that runs the stopwatch
+var intervalId;
+
+// prevents the clock from being sped up unnecessarily
+var clockRunning = false;
 
 var question1 = {
     questiontext:"Who was the Avatar before Aang?", 
@@ -29,6 +34,136 @@ var question3 = {
     correctChoice: "Earth",
     };
 
+    
+function calculateScore() {
+
+    for (var i=0; i<=scoreforAnswers.length;i++) {
+   
+       if (scoreforAnswers[i]==true) {
+   
+           totalcorrectanswers=totalcorrectanswers+1;
+   
+       }
+   
+       else if (scoreforAnswers[i]==false) {
+   
+           totalincorrectanswers=totalincorrectanswers+1;
+   
+       }
+   
+   }
+   totalscore= (totalcorrectanswers/questionlist.length)*100;
+   
+   return totalscore;
+   }
+
+
+    function gameOverChecker(){
+
+        if(questioncounter===(questionlist.length-1)) {
+    
+            $(".QuestionDisplay").hide();
+            $(".QuestionResultDisplay").hide();
+    
+            $(".correctanswer").text(" The correct answer was " + questionlist[questioncounter].correctChoice );
+            questioncounter = questioncounter+1;
+            $(".QuestionDisplay").hide();
+            $(".QuestionResultDisplay").show();
+    
+            calculateScore();
+    
+            $(".finalscorenumber").text(totalscore + "%");
+            $(".totalcorrectanswers").text("Total Correct Answers: " + totalcorrectanswers);
+            $(".totalincorrectanswers").text("Total Wrong Answers: " + totalincorrectanswers);
+    
+            setTimeout(function(){$(".QuestionResultDisplay").hide()}, 5000);
+            setTimeout(function(){$(".FinalScore").show()}, 5000);
+           
+            questioncounter=0;
+    
+            userAnswers=[];
+            scoreforAnswers=[];
+    
+            totalcorrectanswers=0;
+            totalincorrectanswers=0;
+            totalscore=0;
+            
+                
+        }
+    
+        else {
+    
+            $(".correctanswer").text(" The correct answer was " + questionlist[questioncounter].correctChoice );
+            questioncounter = questioncounter+1;
+            $(".QuestionDisplay").hide();
+            $(".QuestionResultDisplay").show();
+            setTimeout($(".QuestionDisplay").show, 10000);
+            setTimeout(function(){$(".QuestionDisplay").show()}, 5000);
+            setTimeout(function(){$(".QuestionResultDisplay").hide()}, 5000);
+            setTimeout(function(){stopwatch.stop()},5000);
+            setTimeout(function(){stopwatch.reset()},5000);
+            setTimeout(function(){stopwatch.start()},5000);
+            setTimeout(function(){displayQuestion()},5000);
+    
+    
+            
+        }
+    }
+    
+    var stopwatch=
+    {
+        time: 5,
+
+        start: function() {
+            console.log("stopwatch started");
+
+            // DONE: Use setInterval to start the count here and set the clock to running.
+            if (!clockRunning) {
+              intervalId = setInterval(stopwatch.count, 1000);
+              clockRunning = true;
+            }
+          },
+          reset: function() {
+
+            stopwatch.time = 5;
+        
+            // DONE: Change the "display" div to "00:00."
+            $("#display").text("5");
+        
+            // DONE: Empty the "laps" div.
+        
+          },
+          stop: function() {
+
+            // DONE: Use clearInterval to stop the count here and set the clock to not be running.
+            clearInterval(intervalId);
+            clockRunning = false;
+          },
+          count: function() {
+
+            if (stopwatch.time<0){
+                stopwatch.stop();
+                stopwatch.reset();
+                $(".result").text(" Out of Time!");
+                scoreforAnswers.push(false);
+                gameOverChecker();
+                
+
+            }
+
+            // DONE: increment time by 1, remember we cant use "this" here.
+            stopwatch.time--;
+            console.log(stopwatch.time);
+               
+            // DONE: Use the variable we just created to show the converted time in the "display" div.
+            $("#display").text(stopwatch.time);
+
+            consolelog(stopwatch.time);
+          },
+
+    }
+
+
 var questionlist=[question1,question2,question3];
 
 var questioncounter=0;
@@ -43,6 +178,10 @@ var totalscore=0;
 $(document).ready(function(){
 
 function displayQuestion() {
+    console.log("starting stopwatch is starting");
+
+    //starttime
+    stopwatch.start();
 
     $(".question").text(questionlist[questioncounter].questiontext)
     $("#A").text(questionlist[questioncounter].answerA);
@@ -52,29 +191,10 @@ function displayQuestion() {
 
 }
 
-function calculateScore() {
 
- for (var i=0; i<=scoreforAnswers.length;i++) {
 
-    if (scoreforAnswers[i]==true) {
 
-        totalcorrectanswers=totalcorrectanswers+1;
 
-    }
-
-    else if (scoreforAnswers[i]==false) {
-
-        totalincorrectanswers=totalincorrectanswers+1;
-
-    }
-
- 
-}
-
-totalscore= (totalcorrectanswers/questionlist.length)*100;
-
-return totalscore;
-}
 
 
 $(".QuestionDisplay").hide();
@@ -97,6 +217,11 @@ $(".startgame").on("click", function(event) {
 
 $(".answerbutton").on("click", function(event) {
 
+    stopwatch.stop();
+    stopwatch.reset();
+
+ 
+
     var currentAnswer=$(this).attr("data-type");
     alert(currentAnswer);
 
@@ -108,9 +233,7 @@ $(".answerbutton").on("click", function(event) {
         scoreforAnswers.push(true);
         $(".result").text(" You were right!");
 
-
     }
-
 
     else {
         $(".result").text(" You were wrong!");
@@ -118,44 +241,8 @@ $(".answerbutton").on("click", function(event) {
 
     }
 
-    if(questioncounter===(questionlist.length-1)) {
+    gameOverChecker();
 
-        $(".QuestionDisplay").hide();
-        $(".QuestionResultDisplay").hide();
-
-        $(".correctanswer").text(" The correct answer was " + questionlist[questioncounter].correctChoice );
-        questioncounter = questioncounter+1;
-        $(".QuestionDisplay").hide();
-        $(".QuestionResultDisplay").show();
-
-        calculateScore();
-
-        $(".finalscorenumber").text(totalscore + "%");
-        $(".totalcorrectanswers").text("Total Correct Answers: " + totalcorrectanswers);
-        $(".totalincorrectanswers").text("Total Wrong Answers: " +totalcorrectanswers);
-
-        setTimeout(function(){$(".QuestionResultDisplay").hide()}, 5000);
-        setTimeout(function(){$(".FinalScore").show()}, 5000);
-       
-        
-        
-            
-    }
-
-    else {
-
-        $(".correctanswer").text(" The correct answer was " + questionlist[questioncounter].correctChoice );
-        questioncounter = questioncounter+1;
-        $(".QuestionDisplay").hide();
-        $(".QuestionResultDisplay").show();
-        setTimeout($(".QuestionDisplay").show, 10000);
-        setTimeout(function(){$(".QuestionDisplay").show()}, 5000);
-        setTimeout(function(){$(".QuestionResultDisplay").hide()}, 5000);
-        displayQuestion();
-    }
-
-
-    
 });
 
 
