c51940dfde19dcffe31a42c460aa9420bc27bccf
LotusGit <lotuslindez@gmail.com>
2019-07-15 23:55:54 -0500

    fixed bug

--- javascript.js
+++ javascript.js
@@ -34,82 +34,92 @@ var question3 = {
     correctChoice: "Earth",
     };
 
-    
-function calculateScore() {
+var question4 = {
+    questiontext:"How can Toph see?", 
+    answerA:"Earthbending", 
+    answerB:"Echolocation", 
+    answerC:"Vibrations in the Air",
+    answerD:"Badgermoles",
+    correctAnswer: "A",
+    correctChoice: "Earthbending",
+    };
 
-    for (var i=0; i<=scoreforAnswers.length;i++) {
-   
-       if (scoreforAnswers[i]==true) {
-   
-           totalcorrectanswers=totalcorrectanswers+1;
-   
-       }
-   
-       else if (scoreforAnswers[i]==false) {
-   
-           totalincorrectanswers=totalincorrectanswers+1;
-   
-       }
-   
-   }
-   totalscore= (totalcorrectanswers/questionlist.length)*100;
-   
-   return totalscore;
-   }
+var question5 = {
+    questiontext:"What is Asami not good at?", 
+    answerA:"Bending", 
+    answerB:"Fighting", 
+    answerC:"Running a Business",
+    answerD:"Engineering",
+    correctAnswer: "A",
+    correctChoice: "Bending",
+    };
 
+var question6 = {
+    questiontext:"What is Uncle Iroh's favorite drink?", 
+    answerA:"Firenation Wine", 
+    answerB:"Jasmine Tea", 
+    answerC:"Coffee",
+    answerD:"Bison Milk",
+    correctAnswer: "B",
+    correctChoice: "Jasmine Tea",
+    };
 
-    function gameOverChecker(){
+    var question7 = {
+        questiontext:"What element does Korra have trouble bending?", 
+        answerA:"Water", 
+        answerB:"Fire", 
+        answerC:"Earth",
+        answerD:"Air",
+        correctAnswer: "D",
+        correctChoice: "Air",
+        };
+    
+    var question8 = {
+        questiontext:"What does Sokka NOT fight with?", 
+        answerA:"Boomerang", 
+        answerB:"Sword", 
+        answerC:"Catapult",
+        answerD:"War Ballon",
+        correctAnswer: "C",
+        correctChoice: "Catapult",
+        };
+    
+    var question9 = {
+        questiontext:"What is the name of the theatre troupe Zuko used to see as a kid?", 
+        answerA:"Kyoshi Island Review", 
+        answerB:"Dragon Wind Players", 
+        answerC:"Ember Island Players",
+        answerD:"Fire Nation Comedy Troupe",
+        correctAnswer: "C",
+        correctChoice: "Ember Island Players",
+        };
 
-        if(questioncounter===(questionlist.length-1)) {
-    
-            $(".QuestionDisplay").hide();
-            $(".QuestionResultDisplay").hide();
-    
-            $(".correctanswer").text(" The correct answer was " + questionlist[questioncounter].correctChoice );
-            questioncounter = questioncounter+1;
-            $(".QuestionDisplay").hide();
-            $(".QuestionResultDisplay").show();
-    
-            calculateScore();
-    
-            $(".finalscorenumber").text(totalscore + "%");
-            $(".totalcorrectanswers").text("Total Correct Answers: " + totalcorrectanswers);
-            $(".totalincorrectanswers").text("Total Wrong Answers: " + totalincorrectanswers);
-    
-            setTimeout(function(){$(".QuestionResultDisplay").hide()}, 5000);
-            setTimeout(function(){$(".FinalScore").show()}, 5000);
-           
-            questioncounter=0;
-    
-            userAnswers=[];
-            scoreforAnswers=[];
-    
-            totalcorrectanswers=0;
-            totalincorrectanswers=0;
-            totalscore=0;
+    var question10 = {
+        questiontext:"What is the best cartoon in the world?", 
+        answerA:"Steven Universe", 
+        answerB:"Simpsons", 
+        answerC:"Pokemon",
+        answerD:"Avatar the Last Airbender",
+        correctAnswer: "A",
+        correctChoice: "Steven Universe",
+        };
+
+        $(document).ready(function(){
+
+            function displayQuestion() {
+                console.log("starting stopwatch is starting");
             
-                
-        }
-    
-        else {
-    
-            $(".correctanswer").text(" The correct answer was " + questionlist[questioncounter].correctChoice );
-            questioncounter = questioncounter+1;
-            $(".QuestionDisplay").hide();
-            $(".QuestionResultDisplay").show();
-            setTimeout($(".QuestionDisplay").show, 10000);
-            setTimeout(function(){$(".QuestionDisplay").show()}, 5000);
-            setTimeout(function(){$(".QuestionResultDisplay").hide()}, 5000);
-            setTimeout(function(){stopwatch.stop()},5000);
-            setTimeout(function(){stopwatch.reset()},5000);
-            setTimeout(function(){stopwatch.start()},5000);
-            setTimeout(function(){displayQuestion()},5000);
-    
-    
+                //starttime
+                stopwatch.start();
             
-        }
-    }
-    
+                $(".question").text(questionlist[questioncounter].questiontext)
+                $("#A").text(questionlist[questioncounter].answerA);
+                $("#B").text(questionlist[questioncounter].answerB);
+                $("#C").text(questionlist[questioncounter].answerC);
+                $("#D").text(questionlist[questioncounter].answerD);
+            
+            }
+
     var stopwatch=
     {
         time: 5,
@@ -139,16 +149,71 @@ function calculateScore() {
             clearInterval(intervalId);
             clockRunning = false;
           },
+
           count: function() {
 
-            if (stopwatch.time<0){
-                stopwatch.stop();
-                stopwatch.reset();
-                $(".result").text(" Out of Time!");
-                scoreforAnswers.push(false);
-                gameOverChecker();
-                
+            if (stopwatch.time<1){
 
+                stopwatch.stop();
+                if(questioncounter===(questionlist.length-1)) {
+
+                    $(".QuestionDisplay").hide();
+                    $(".QuestionResultDisplay").hide();
+            
+                    $(".correctanswer").text(" The correct answer was " + questionlist[questioncounter].correctChoice );
+                    questioncounter = questioncounter+1;
+                    $(".QuestionDisplay").hide();
+                    $(".QuestionResultDisplay").show();
+            
+                    calculateScore();
+            
+                    $(".finalscorenumber").text(totalscore + "%");
+                    $(".totalcorrectanswers").text("Total Correct Answers: " + totalcorrectanswers);
+                    $(".totalincorrectanswers").text("Total Wrong Answers: " + totalincorrectanswers);
+            
+                    setTimeout(function(){$(".QuestionResultDisplay").hide()}, 5000);
+                    setTimeout(function(){$(".FinalScore").show()}, 5000);
+            
+            
+                    if(totalscore < 60){
+                        $(".gameovermessage").text(" You need to brush up on your Avatar knowledge");
+                
+                        }
+                
+                        else if(totalscore >= 60 && totalscore< 80 ){
+                            $(".gameovermessage").text(" Pretty Good!");
+                    
+                            }
+                
+                        else {
+                            $(".gameovermessage").text(" You are an Avatar Master!");
+                    
+                            }
+                    stopwatch.stop();
+                        
+                }
+            
+                else {
+            
+                    $(".correctanswer").text(" The correct answer was " + questionlist[questioncounter].correctChoice );
+            
+            
+                    questioncounter = questioncounter+1;
+                    $(".QuestionDisplay").hide();
+                    $(".QuestionResultDisplay").show();
+                    setTimeout($(".QuestionDisplay").show, 10000);
+                    setTimeout(function(){$(".QuestionDisplay").show()}, 5000);
+                    setTimeout(function(){$(".QuestionResultDisplay").hide()}, 5000);
+                    setTimeout(function(){stopwatch.stop()},5000);
+                    setTimeout(function(){stopwatch.reset()},5000);
+                    setTimeout(function(){displayQuestion()},5000);
+            
+                                
+            
+                    
+                }
+            
+    
             }
 
             // DONE: increment time by 1, remember we cant use "this" here.
@@ -158,13 +223,13 @@ function calculateScore() {
             // DONE: Use the variable we just created to show the converted time in the "display" div.
             $("#display").text(stopwatch.time);
 
-            consolelog(stopwatch.time);
+            console.log(stopwatch.time);
           },
 
     }
 
 
-var questionlist=[question1,question2,question3];
+var questionlist=[question1,question2,question3,question4,question5,question6,question7,question8,question9,question10];
 
 var questioncounter=0;
 
@@ -175,26 +240,29 @@ var totalcorrectanswers=0;
 var totalincorrectanswers=0;
 var totalscore=0;
 
-$(document).ready(function(){
 
-function displayQuestion() {
-    console.log("starting stopwatch is starting");
 
-    //starttime
-    stopwatch.start();
+function calculateScore() {
 
-    $(".question").text(questionlist[questioncounter].questiontext)
-    $("#A").text(questionlist[questioncounter].answerA);
-    $("#B").text(questionlist[questioncounter].answerB);
-    $("#C").text(questionlist[questioncounter].answerC);
-    $("#D").text(questionlist[questioncounter].answerD);
+ for (var i=0; i<=scoreforAnswers.length;i++) {
+
+    if (scoreforAnswers[i]===true) {
+
+        totalcorrectanswers=totalcorrectanswers+1;
+
+    }
+
+    else if (scoreforAnswers[i]===false) {
+
+        totalincorrectanswers=totalincorrectanswers+1;
+
+    }
+
+ 
+}
+totalscore= (totalcorrectanswers/questionlist.length)*100;
 
 }
-
-
-
-
-
 
 
 $(".QuestionDisplay").hide();
@@ -216,24 +284,26 @@ $(".startgame").on("click", function(event) {
 
 
 $(".answerbutton").on("click", function(event) {
-
     stopwatch.stop();
-    stopwatch.reset();
+    stopwatch.reset(); 
 
  
 
     var currentAnswer=$(this).attr("data-type");
-    alert(currentAnswer);
-
-    userAnswers.push(currentAnswer);
+     userAnswers.push(currentAnswer);
 
     
     
     if (currentAnswer===questionlist[questioncounter].correctAnswer) {
         scoreforAnswers.push(true);
         $(".result").text(" You were right!");
+       
+
 
     }
+
+
+
 
     else {
         $(".result").text(" You were wrong!");
@@ -241,8 +311,67 @@ $(".answerbutton").on("click", function(event) {
 
     }
 
-    gameOverChecker();
 
+    if(questioncounter===(questionlist.length-1)) {
+
+        $(".QuestionDisplay").hide();
+        $(".QuestionResultDisplay").hide();
+
+        $(".correctanswer").text(" The correct answer was " + questionlist[questioncounter].correctChoice );
+        questioncounter = questioncounter+1;
+        $(".QuestionDisplay").hide();
+        $(".QuestionResultDisplay").show();
+
+        calculateScore();
+
+        $(".finalscorenumber").text(totalscore + "%");
+        $(".totalcorrectanswers").text("Total Correct Answers: " + totalcorrectanswers);
+        $(".totalincorrectanswers").text("Total Wrong Answers: " + totalincorrectanswers);
+
+        setTimeout(function(){$(".QuestionResultDisplay").hide()}, 5000);
+        setTimeout(function(){$(".FinalScore").show()}, 5000);
+
+
+        if(totalscore < 60){
+            $(".gameovermessage").text(" You need to brush up on your Avatar knowledge");
+    
+            }
+    
+            else if(totalscore >= 60 && totalscore< 80 ){
+                $(".gameovermessage").text(" Pretty Good!");
+        
+                }
+    
+            else {
+                $(".gameovermessage").text(" You are an Avatar Master!");
+        
+                }
+        stopwatch.stop();
+            
+    }
+
+    else {
+
+        $(".correctanswer").text(" The correct answer was " + questionlist[questioncounter].correctChoice );
+
+
+        questioncounter = questioncounter+1;
+        $(".QuestionDisplay").hide();
+        $(".QuestionResultDisplay").show();
+        setTimeout($(".QuestionDisplay").show, 10000);
+        setTimeout(function(){$(".QuestionDisplay").show()}, 5000);
+        setTimeout(function(){$(".QuestionResultDisplay").hide()}, 5000);
+        setTimeout(function(){stopwatch.stop()},5000);
+        setTimeout(function(){stopwatch.reset()},5000);
+        setTimeout(function(){displayQuestion()},5000);
+
+                    
+
+        
+    }
+
+
+    
 });
 
 
